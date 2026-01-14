784fa22f5aec9405f29fa142bd0b63ea70bc9e5d
EZednanref <fernandeze@cy-tech.fr>
2026-01-14 14:53:34 +0100

    ajout sécurité : hash mot de passe + classes UserLogin et UserCreate

--- backend/app/security.py
+++ backend/app/security.py
@@ -5,14 +5,17 @@ from passlib.context import CryptContext
 SECRET_KEY = "151515"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 10
-# TODO: add the hashing inside the functions
-pwd_context = None  # No longer used
+
+pwd_context = CryptContext(
+    schemes=["bcrypt"],
+    deprecated="auto"
+)
 
 def hash_password(password: str) -> str:
-    return password
+    return pwd_context.hash(password)
 
 def verify_password(password: str, hashed: str) -> bool:
-    return password == hashed
+    return pwd_context.verify(password, hashed)
 
 # Token
 def create_access_token(data: dict):
@@ -20,4 +23,3 @@ def create_access_token(data: dict):
     expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode.update({"exp": expire})
     return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
-
